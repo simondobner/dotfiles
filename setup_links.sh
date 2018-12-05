7dd3d300b149a204c9abbdedc5ca82beec20189e
Simon Dobner <simon.dobner@psma.com.au>
2018-12-06 10:13:42 +1100

    Add iterm2 config

--- setup_links.sh
+++ setup_links.sh
@@ -1,1 +1,8 @@
 ln -sv ~/dotfiles/.vimrc  ~/
+
+# setup iterm to read pref from the dotfiles dir
+
+# Specify the preferences directory - from http://stratus3d.com/blog/2015/02/28/sync-iterm2-profile-with-dotfiles-repository/
+defaults write com.googlecode.iterm2.plist PrefsCustomFolder -string "~/dotfiles"
+# Tell iTerm2 to use the custom preferences in the directory
+defaults write com.googlecode.iterm2.plist LoadPrefsFromCustomFolder -bool true
